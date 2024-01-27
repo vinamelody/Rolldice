3b9bf96d114eb26f65cdd381485ebb23094ea019
Vina Melody <vina_melody@yahoo.com>
2024-01-27 12:15:42 +0800

    Setup text

--- Rolldice/ContentView.swift
+++ Rolldice/ContentView.swift
@@ -11,41 +11,18 @@ import RealityKitContent
 
 struct ContentView: View {
 
-    @State private var showImmersiveSpace = false
-    @State private var immersiveSpaceIsShown = false
-
     @Environment(\.openImmersiveSpace) var openImmersiveSpace
     @Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
 
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
-
-            Toggle("Show Immersive Space", isOn: $showImmersiveSpace)
-                .toggleStyle(.button)
-                .padding(.top, 50)
+            Text("🎲")
+                .foregroundStyle(.yellow)
+                .font(.custom("Menlo", size: 100))
+                .bold()
         }
-        .padding()
-        .onChange(of: showImmersiveSpace) { _, newValue in
-            Task {
-                if newValue {
-                    switch await openImmersiveSpace(id: "ImmersiveSpace") {
-                    case .opened:
-                        immersiveSpaceIsShown = true
-                    case .error, .userCancelled:
-                        fallthrough
-                    @unknown default:
-                        immersiveSpaceIsShown = false
-                        showImmersiveSpace = false
-                    }
-                } else if immersiveSpaceIsShown {
-                    await dismissImmersiveSpace()
-                    immersiveSpaceIsShown = false
-                }
-            }
+        .task {
+            await openImmersiveSpace(id: "ImmersiveSpace")
         }
     }
 }
--- Rolldice/RolldiceApp.swift
+++ Rolldice/RolldiceApp.swift
@@ -13,6 +13,7 @@ struct RolldiceApp: App {
         WindowGroup {
             ContentView()
         }
+        .defaultSize(width: 100, height: 100)
 
         ImmersiveSpace(id: "ImmersiveSpace") {
             ImmersiveView()
