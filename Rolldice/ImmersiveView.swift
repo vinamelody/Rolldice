575ae169760588f05db6332145466e17d12d1c62
Vina Melody <vina_melody@yahoo.com>
2024-01-27 12:11:05 +0800

    Add a dice

--- Rolldice/ImmersiveView.swift
+++ Rolldice/ImmersiveView.swift
@@ -12,9 +12,13 @@ import RealityKitContent
 struct ImmersiveView: View {
     var body: some View {
         RealityView { content in
-            // Add the initial RealityKit content
-            if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
-                content.add(scene)
+            if let diceModel = try? await Entity(named: "dice"),
+               let dice = diceModel.children.first?.children.first {
+
+                dice.scale = [0.1, 0.1, 0.1]
+                dice.position.y = 0.5
+                dice.position.z = -1
+                content.add(dice)
             }
         }
     }
