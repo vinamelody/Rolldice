ce83d7c5d9997b580d1537eedb53ad3c649f6776
Vina Melody <vina_melody@yahoo.com>
2024-01-27 12:29:15 +0800

    Add floor and gesture

--- Rolldice/ImmersiveView.swift
+++ Rolldice/ImmersiveView.swift
@@ -12,15 +12,45 @@ import RealityKitContent
 struct ImmersiveView: View {
     var body: some View {
         RealityView { content in
+            let floor = ModelEntity(mesh: .generatePlane(width: 50, depth: 50), materials: [OcclusionMaterial()])
+            floor.generateCollisionShapes(recursive: false)
+            floor.components[PhysicsBodyComponent.self] = .init(massProperties: .default, mode: .static)
+
+            content.add(floor)
+
             if let diceModel = try? await Entity(named: "dice"),
                let dice = diceModel.children.first?.children.first {
 
                 dice.scale = [0.1, 0.1, 0.1]
                 dice.position.y = 0.5
                 dice.position.z = -1
+
+                dice.generateCollisionShapes(recursive: false)
+                dice.components.set(InputTargetComponent())
+
+                dice.components[PhysicsBodyComponent.self] = .init(
+                    PhysicsBodyComponent(
+                        massProperties: .default,
+                        material: .generate(staticFriction: 0.8, dynamicFriction: 0.5, restitution: 0.1),
+                        mode: .dynamic))
+
+                dice.components[PhysicsMotionComponent.self] = .init()
                 content.add(dice)
             }
         }
+        .gesture(dragGesture)
+    }
+
+    var dragGesture: some Gesture {
+        DragGesture()
+            .targetedToAnyEntity()
+            .onChanged { value in
+                value.entity.position = value.convert(value.location3D, from: .local, to: value.entity.parent!)
+                value.entity.components[PhysicsBodyComponent.self]?.mode = .kinematic
+            }
+            .onEnded { value in
+                value.entity.components[PhysicsBodyComponent.self]?.mode = .dynamic
+            }
     }
 }
 
